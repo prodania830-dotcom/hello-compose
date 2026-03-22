506b27c211a50c0c600aeea3ec61e515b79d1fe9
Danik <prodania830@gmail.com>
2026-03-22 22:07:16 +0300

    Initial commit

---
+++ app/src/main/java/com/example/hellocompose/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.hellocompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.tooling.preview.Preview
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            HelloComposeApp()
+        }
+    }
+}
+
+@Composable
+fun HelloComposeApp() {
+    MaterialTheme {
+        var name by remember { mutableStateOf("Студент") }
+        Surface {
+            Column {
+                Text(
+                    text = "Привіт, $name!",
+                    style = MaterialTheme.typography.headlineMedium
+                )
+                Button(onClick = { name = "Світ" }) {
+                    Text("Натисни")
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewHello() {
+    HelloComposeApp()
+}
